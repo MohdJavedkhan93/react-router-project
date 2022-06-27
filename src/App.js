1732bf70d9588ca6dd4faf0fb540f6838bf0c1cf
MohdJavedkhan93 <javedk.7491@gmail.com>
2022-06-27 09:11:28 +0530

    react router project upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router,
+Switch,
+Route,
+} from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/home">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
